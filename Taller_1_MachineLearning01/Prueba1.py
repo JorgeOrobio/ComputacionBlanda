5e2f698ade275065d673226c082be360cfed5edc
Jorge Orobio Auz <j.orobio@utp.edu.co>
2020-08-26 15:31:03 -0500

    taller1

---
+++ Taller_1_MachineLearning01/Prueba1.py
@@ -0,0 +1,14 @@
+# Se importa la librería numpy
+import numpy as np
+
+# Se crea un vector (array) con seis elementos
+a = np.array([0,1,2,3,4,5])
+
+# Se imprime el array ... a
+print(a, '\n')
+
+# Número de dimensiones del array
+print(a.ndim, '\n')
+
+# Número de elementos del array
+print(a.shape)
